44f7bb77f5f5d9e7e4b86a0eb8922222831c9161
VasyaTarn <vasatopktoneverittomuvlob@gmail.com>
2024-02-10 16:57:25 +0200

    Add project files.

---
+++ asp_hw_1/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_hw_1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Restaurant> Restaurants { get; set; }
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Restaurants = new List<Restaurant>
+            {
+                new Restaurant { Id = 1, Name = "Central", Location = "Lima, Peru", Chef = "Virgilio Martinez" },
+                new Restaurant { Id = 2, Name = "Disfrutar", Location = "Barcelona, ​​Spain", Chef = "Oriol Castro" },
+                new Restaurant { Id = 3, Name = "Diverxo", Location = "Madrid, Spain", Chef = "David Muñoz" },
+            };
+        }
+
+        public class Restaurant
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Location { get; set; }
+            public string Chef { get; set; }
+
+        }
+    }
+}
---
+++ asp_hw_1/Pages/Restaurant.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_hw_1.Pages
+{
+    public class RestaurantModel : PageModel
+    {
+        public int RestaurantId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Chef { get; set; }
+
+
+        public void OnGet(int restaurantId, string name, string location, string chef)
+        {
+            RestaurantId = restaurantId;
+            Name = name;
+            Location = location;
+            Chef = chef;
+        }
+    }
+}
